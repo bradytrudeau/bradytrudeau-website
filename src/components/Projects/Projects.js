fa50c8877c4def6a79663d277fb8308ead81ada1
Brady Trudeau <bradytrudeau@gmail.com>
2020-12-21 14:18:19 -0600

    Updated projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Projects.css';
 import click from '../../Images/click.png';
-import movies from '../../Images/movies.png';
+import steps from '../../Images/steps.png';
 
 
 
@@ -15,28 +15,29 @@ class Projects extends Component {
                     <a href="https://github.com/bradytrudeau/click-app" target="_blank">
                         <img src={click} className="projectVideo"></img>
                     </a>
-                    <a href="https://github.com/bradytrudeau/movie-sagas" target="_blank">
-                        <img src={movies} className="projectVideo"></img>
+                    <a href="https://github.com/Playworks/playworks-step-challenge" target="_blank">
+                        <img src={steps} className="projectVideo"></img>
                     </a>
                   </div>
                   <div className="right-projects">
-                  <h3 className="projectsH3Top">Click! Metronome App</h3>
-                  <p className="projectsP">
-                    As a musician, I've used hundreds of different tools to help hone my craft. 
-                    Of all of these tools, the most important has been the metronome. 
-                    I've used dozens of different kinds of metronomes, 
-                    but have always had an issue with their inability to save a setlist of tempos  
-                    and the limited number of sound options they have. 
-                    For this reason, I've created the Click! Metronome App.
-                    <a className="videoLink" href="https://youtu.be/FPe3gaOlHdg" target="_blank"> Click here</a> for a demo of the app. Enjoy!
-                  </p>
-                  <h3 className="projectsH3Bottom">Popular Movies App</h3>
+                    <h3 className="projectsH3Top">Click! Metronome App</h3>
+                    <p className="projectsP">
+                      As a musician, I've used hundreds of different tools to help hone my craft. 
+                      Of all of these tools, the most important has been the metronome. 
+                      I've used dozens of different kinds of metronomes, 
+                      but have always had an issue with their inability to save a setlist of tempos  
+                      and the limited number of sound options they have. 
+                      For this reason, I've created the Click! Metronome App.
+                      <a className="videoLink" href="https://youtu.be/FPe3gaOlHdg" target="_blank"> Click here</a> for a demo of the app. Enjoy!
+                    </p>
+                    <h3 className="projectsH3Bottom">Playworks Step Challenge App</h3>
                     <p className="projectsP"> 
-                        The entertainment industry is a multi-billion dollar industry that has a ton of
-                        influence on people around the world. The Popular Movies App is an app designed to help
-                        fans of movies keep track of their favorite flicks. It features movie posters, 
-                        movie titles, genres, and a description of each movie. Users can input their favorites
-                        into the app for quick recall later. 
+                      The Playworks Step Challenge App is a full stack mobile-first application that allows users to 
+                      create or join a team, log steps, and view team and user statistics. 
+                      Application allows users to upload images based on challenges that are assigned by the Playworks admins. 
+                      These images will be displayed in multiple image feeds to encourage user interaction. 
+                      When a team is created by a user, that user is a “captain” giving that user the ability to remove users 
+                      from the team and approve or deny photos that are uploaded by the team members. 
                     </p>
                   </div>
               </div>
--- src/components/ProjectsMobile/ProjectsMobile.js
+++ src/components/ProjectsMobile/ProjectsMobile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './ProjectsMobile.css';
 import click from '../../Images/click.png';
-import movies from '../../Images/movies.png';
+import steps from '../../Images/steps.png';
 
 
 
@@ -25,16 +25,17 @@ class ProjectsMobile extends Component {
                       For this reason, I've created the Click! Metronome App.
                     <a className="videoLink" href="https://youtu.be/FPe3gaOlHdg" target="_blank"> Click here</a> for a demo of the app. Enjoy!
                     </p>
-                    <a href="https://github.com/bradytrudeau/movie-sagas" target="_blank">
-                        <img src={movies} className="projectVideoMobile"></img>
+                    <a href="https://github.com/Playworks/playworks-step-challenge" target="_blank">
+                        <img src={steps} className="projectVideoMobile"></img>
                     </a>
-                    <h3 className="projectsH3BottomMobile">Popular Movies App</h3>
+                    <h3 className="projectsH3BottomMobile">Playworks Step Challenge App</h3>
                     <p className="projectsPMobile"> 
-                        The entertainment industry is a multi-billion dollar industry that has a ton of
-                        influence on people around the world. The Popular Movies App is an app designed to help
-                        fans of movies keep track of their favorite flicks. It features movie posters, 
-                        movie titles, genres, and a description of each movie. Users can input their favorites
-                        into the app for quick recall later. 
+                    The Playworks Step Challenge App is a full stack mobile-first application that allows users to 
+                    create or join a team, log steps, and view team and user statistics. 
+                    Application allows users to upload images based on challenges that are assigned by the Playworks admins. 
+                    These images will be displayed in multiple image feeds to encourage user interaction. 
+                    When a team is created by a user, that user is a “captain” giving that user the ability to remove users 
+                    from the team and approve or deny photos that are uploaded by the team members. 
                     </p>
                   </div>
                   <div className="right-projectsMobile">
